adb81690931d6a14fbcb7b2927fe5b55909c8159
hechfx <60321993+hechfx@users.noreply.github.com>
2020-10-26 19:19:41 -0300

    Derp

--- src/main/kotlin/com/hechfx/project/commands/misc/InviteCommand.kt
+++ src/main/kotlin/com/hechfx/project/commands/misc/InviteCommand.kt
@@ -9,7 +9,7 @@ class InviteCommand: CommandBuilder("invite", arrayOf("adicionar", "add"), "misc
     override fun onCommand(event: GuildMessageReceivedEvent, context: CommandContext) {
         context.sendMessage(
             Reply(
-                "Obrigado por querer me adicionar, basta apertar no link abaixo!\n\n"
+                "Obrigado por querer me adicionar, basta apertar no link abaixo!\n\nhttps://discord.com/api/oauth2/authorize?client_id=758128536908988436&permissions=51264&scope=bot"
             ).build(context)
         )
     }
