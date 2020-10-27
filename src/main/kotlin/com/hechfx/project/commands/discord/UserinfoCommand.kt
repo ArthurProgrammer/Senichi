a2c066ad8a58be5d285757fa09e3ad3e189bf4dd
hechfx <60321993+hechfx@users.noreply.github.com>
2020-10-26 22:11:18 -0300

    Update UserinfoCommand.kt

--- src/main/kotlin/com/hechfx/project/commands/discord/UserinfoCommand.kt
+++ src/main/kotlin/com/hechfx/project/commands/discord/UserinfoCommand.kt
@@ -5,7 +5,7 @@ import com.hechfx.project.commands.CommandContext
 import net.dv8tion.jda.api.EmbedBuilder
 import java.awt.Color
 
-class UserinfoCommand: CommandBuilder("userinfo", arrayOf("infouser"), "discord") {
+class UserinfoCommand: CommandBuilder("userinfo", arrayOf("infouser", "memberinfo"), "discord") {
     override fun onCommand(context: CommandContext) {
         val user = context.message.mentionedUsers.firstOrNull { it.asMention == context.rawArgs.getOrNull(0)?.replace("!", "") } ?: context.rawArgs.getOrNull(0)?.toLong()?.let {
             context.jda.getUserById(it)
@@ -16,13 +16,13 @@ class UserinfoCommand: CommandBuilder("userinfo", arrayOf("infouser"), "discord"
         val embed = EmbedBuilder()
             .setTitle("Informações do Usuário")
             .setThumbnail(userAvatarUrl)
-            .setFooter("Command executed by ${context.author.asTag}", context.author.effectiveAvatarUrl + "?size=4096")
+            .setFooter("Command executed by ${context.author.asTag}", context.author.effectiveAvatarUrl)
             .addField("Tag do Discord", user.asTag, true)
             .addField("ID do usuário", user.id, true)
-            .addField("Conta foi criada", "há ${user.timeCreated.year} anos, ${user.timeCreated.monthValue} meses e ${user.timeCreated.dayOfMonth} dias", true)
+            .addField("Conta foi criada", "${user.timeCreated.dayOfMonth}/${user.timeCreated.monthValue}/${user.timeCreated.year} ${user.timeCreated.hour}", true)
             if (context.guild.isMember(user)) {
-                embed.addField("Entrou aqui", "há ${context.guild.getMemberById(user.id)?.timeJoined}", true)
                 val member = context.guild.getMember(user)
+                embed.addField("Entrou aqui", "há ${member?.timeJoined?.dayOfMonth}/${member?.timeJoined?.monthValue}/${member?.timeCreated?.year} ${member?.timeCreated?.hour}", true)
                 embed.setColor(member?.color ?: Color(114, 137, 218))
             } else {
                 embed.setColor(Color(114, 137, 218))
